1e0ff3aa4ef0f0fd65a7925e6c9956b41fbdc63f
Annika <Annika.johnson@protonmail.com>
2024-03-26 04:39:01 -0500

    blog update splash text update

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ var a = ['<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Undefined...</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >l33t!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Music by C418!</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Pure Virtue Signalling</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >I\'d cut off all my fingers just to touch you</h2></a>',
 
 ];
-var x = Math.floor(Math.random() * 6);
+var x = Math.floor(Math.random() * 8);
 var string = a[x];
 document.getElementById("string").innerHTML = string;
