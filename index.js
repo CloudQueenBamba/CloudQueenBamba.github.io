05b0cee2e851f79dfab519f6ef541a0d5e59f451
Annika <Annika.johnson@protonmail.com>
2024-03-26 19:26:10 -0500

    splash update. blog update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var a = ['<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Wrists are for girls, I\'m slitting my throat</h2></a>', 
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Minecraft!</h2></a>', 
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Think it! Dream it! Do it!</h2></a>',
@@ -7,8 +6,10 @@ var a = ['<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Music by C418!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Pure Virtue Signalling</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >I\'d cut off all my fingers just to touch you</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >uh oh! bad decision, mark!</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >ravioli ravioli what\'s in the pocketoli</h2></a>',
 
 ];
-var x = Math.floor(Math.random() * 8);
+var x = Math.floor(Math.random() * 10);
 var string = a[x];
 document.getElementById("string").innerHTML = string;
