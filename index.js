ec9d261e9bddd130d86fff267f98c3ac35dd7ac2
Annika <Annika.johnson@protonmail.com>
2024-04-01 07:57:41 -0500

    lily crochet + blog

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var a = ['<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Wrists are for girls, I\'m slitting my throat</h2></a>', 
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Minecraft!</h2></a>', 
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Think it! Dream it! Do it!</h2></a>',
-'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Undefined...</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Think it! Dream it! Do it!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >l33t!</h2></a>',
-'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Music by C418!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Pure Virtue Signalling</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >I\'d cut off all my fingers just to touch you</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >uh oh! bad decision, mark!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >ravioli ravioli what\'s in the pocketoli</h2></a>',
-
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >i love backdoors! thank you intel very cool!</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >i can fix him...</h2></a>',
 ];
-var x = Math.floor(Math.random() * 10);
+var x = Math.floor(Math.random() * 11);
 var string = a[x];
 document.getElementById("string").innerHTML = string;
