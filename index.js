59632561dce1a69f32bc9f94b489fd5f1bb677ae
Annika <Annika.johnson@protonmail.com>
2024-04-14 00:22:22 -0500

    blog

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ var a = ['<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Think it! Dream it! Do it!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >l33t!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Pure Virtue Signalling</h2></a>',
-'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >I\'d cut off all my fingers just to touch you</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >uh oh! bad decision, mark!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >ravioli ravioli what\'s in the pocketoli</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >i love backdoors! thank you intel very cool!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >i can fix him...</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none; font-size: 46px;" >the perfectionist in me vehemently hates this website</h2></a>',
 ];
-var x = Math.floor(Math.random() * 12);
+var x = Math.floor(Math.random() * 11);
 var string = a[x];
 document.getElementById("string").innerHTML = string;
