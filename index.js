4a61d63bbb17d778993ca8669ad26ae4a1a3a62f
Annika <Annika.johnson@protonmail.com>
2024-04-19 18:25:06 -0500

    content update

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var a = ['<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Wrists are for girls, I\'m slitting my throat</h2></a>', 
-'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Minecraft!</h2></a>', 
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Think it! Dream it! Do it!</h2></a>',
-'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Think it! Dream it! Do it!</h2></a>',
-'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >l33t!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Pure Virtue Signalling</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >uh oh! bad decision, mark!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >ravioli ravioli what\'s in the pocketoli</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >i love backdoors! thank you intel very cool!</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >i can fix him...</h2></a>',
 '<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none; font-size: 46px;" >the perfectionist in me vehemently hates this website</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >LEEEEENNNNYYYYYY MAH BOY</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >or is it crochet queen?</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none;" >Always know where your towel is</h2></a>',
+'<a href="index.html"><h2 style="color:rgb(252,152,103);text-decoration: none; font-size: 35px;" >bulimia is for wimps, normalize hacking off body parts with an axe</h2></a>',
 ];
-var x = Math.floor(Math.random() * 11);
+var x = Math.floor(Math.random() * 12);
 var string = a[x];
 document.getElementById("string").innerHTML = string;
